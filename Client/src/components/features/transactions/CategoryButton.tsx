08550c3170a7cb1eaee0b1b064ca29b43da0b44b
Chikezie <chixide12@gmail.com>
2024-12-16 23:58:33 +0000

    refactor: - made the categories sorted

--- Client/src/components/features/transactions/CategoryButton.tsx
+++ Client/src/components/features/transactions/CategoryButton.tsx
@@ -27,21 +27,23 @@ export default function CategoryButton({column}: {column:  Column<Transaction, u
                     {column.id}
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="border-0 bg-neutral-700/[0.7] flex justify-center flex-wrap max-w-[23rem] gap-1 py-3 backdrop-blur-sm" align="center">
-                {Array.from<string, string>(column.getFacetedUniqueValues().keys(), (c) => c.toLowerCase()).map(category => (
-                    <Toggle
-                        className="capitalize data-[state=on]:hover:bg-secondary/[0.2] backdrop-blur-sm data-[state=on]:backdrop-blur-sm data-[state=on]:bg-secondary/[0.3]
-                        data-[state=on]:text-secondary data-[state=on]:text-xs text-xs bg-muted/[0.3] text-neutral-300 hover:bg-muted/[0.2] hover:text-neutral-300"
-                        key={category + " Toggle"}
-                        pressed={isFiltered(category)}
-                        onPressedChange={() => {
-                            const updated = curFilter
-                            updated[category] = !updated[category];
-                            column.setFilterValue(updated)
-                        }}
-                    >
-                        {category}
-                    </Toggle>
+            <DropdownMenuContent className="border-0 bg-neutral-700/[0.7] flex justify-center flex-wrap max-w-[21rem] gap-1 py-3 backdrop-blur-sm" align="center">
+                {Array.from<string, string>(column.getFacetedUniqueValues().keys(), (c) => c.toLowerCase())
+                    .sort()
+                    .map(category => (
+                        <Toggle
+                            className="capitalize data-[state=on]:hover:bg-secondary/[0.2] backdrop-blur-sm data-[state=on]:backdrop-blur-sm data-[state=on]:bg-secondary/[0.3]
+                            data-[state=on]:text-secondary data-[state=on]:text-xs text-xs bg-muted/[0.3] text-neutral-300 hover:bg-muted/[0.2] hover:text-neutral-300"
+                            key={category + " Toggle"}
+                            pressed={isFiltered(category)}
+                            onPressedChange={() => {
+                                const updated = curFilter
+                                updated[category] = !updated[category];
+                                column.setFilterValue(updated)
+                            }}
+                        >
+                            {category}
+                        </Toggle>
                 ))}
             </DropdownMenuContent>
         </DropdownMenu>
