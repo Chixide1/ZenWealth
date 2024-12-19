479b60b2516a8cecfc641785152a6f203ea98b02
Chikezie <Chixide12@gmail.com>
2024-12-19 15:19:01 +0000

    style: - added calendar to date filter button

--- Client/src/components/features/transactions/DateFilterButton.tsx
+++ Client/src/components/features/transactions/DateFilterButton.tsx
@@ -1,12 +1,18 @@
 ﻿import FilterButton from "@/components/shared/FilterButton.tsx";
 import {Transaction} from "@/components/features/transactions/TransactionColumns.tsx";
 import { Column } from "@tanstack/react-table";
+import { DateTimePicker } from "@/components/core/date-time-picker";
+import { useState } from "react";
 
 
 export default function DateFilterButton({column}: { column: Column<Transaction, unknown> }){
+    const [date, setDate] = useState<Date | undefined>(undefined);
+    
     return (
         <FilterButton column={column}>
-            <div></div>
+            <div>
+                <DateTimePicker value={date} onChange={setDate} className="w-[280px]" />
+            </div>
         </FilterButton>
     )
 }
