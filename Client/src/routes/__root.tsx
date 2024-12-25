1578fe9d61139f521fc5c0a4187fbf2071056222
Chikezie <chixide12@gmail.com>
2024-12-25 21:54:05 +0000

    feat: - added the loading component as a pending component to the root layout - renamed ConnectionStatus.tsx to Layout - added sidebar package from shadcn

--- Client/src/routes/__root.tsx
+++ Client/src/routes/__root.tsx
@@ -1,7 +1,9 @@
 ﻿import { createRootRoute, Outlet, } from '@tanstack/react-router'
 import {Auth} from "@/components/shared/Auth.tsx";
+import Loading from '@/components/shared/Loading';
 
 export const Route = createRootRoute({
+    pendingComponent: Loading,
     beforeLoad: Auth,
     component: Root
 })
--- Client/src/routes/_layout.tsx
+++ Client/src/routes/_layout.tsx
@@ -1,6 +1,53 @@
-﻿import { ConnectionStatus } from '@/components/shared/ConnectionStatus.tsx'
-import { createFileRoute } from '@tanstack/react-router'
+﻿import { createFileRoute } from '@tanstack/react-router'
+import {LinkStart} from "@/components/features/link/LinkStart.tsx";
+import { Outlet } from "@tanstack/react-router";
+import axios, { AxiosResponse } from "axios";
+import {useEffect, useState } from "react";
+import TransactionsProvider from "@/providers/TransactionsProvider.tsx";
+import Loading from "@/components/shared/Loading.tsx";
+// import { SidebarProvider, SidebarTrigger } from "@/components/core/sidebar"
+// import { AppSidebar } from "@/components/shared/AppSidebar"
 
 export const Route = createFileRoute('/_layout')({
-  component: ConnectionStatus,
+    component: Layout,
 })
+
+export function Layout() {
+    const [connected, setConnected] = useState<boolean | null>(null)
+    const backend = `${import.meta.env.VITE_ASPNETCORE_URLS}/api`
+
+    useEffect(() => {
+        async function fetchStatus(){
+            await axios
+                .get(`${backend}/IsAccountConnected`, { withCredentials: true })
+                .then((response: AxiosResponse<{ connected: boolean }>) => {
+                    setConnected(response.data.connected)
+                })
+                .catch((error: AxiosResponse) => {
+                    console.error('Error occurred', error)
+                })
+        }
+        fetchStatus()
+    }, []);
+
+    if(connected == null){
+        return <Loading/>
+    }
+
+    if(!connected){
+        return (
+            <div className={"w-full h-screen flex items-center justify-center"}>
+                <LinkStart />
+            </div>
+        )
+    }
+
+    return (
+        <TransactionsProvider>
+            <main>
+                <Outlet />
+            </main>
+        </TransactionsProvider>
+    )
+}
+
