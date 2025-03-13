dffa85072c09778cdcafdec2efc337cc881318fe
Chikezie <Chixide12@gmail.com>
2025-03-13 10:20:45 +0000

    _

--- Client/src/routes/_layout/analytics.tsx
+++ Client/src/routes/_layout/analytics.tsx
@@ -1,9 +1,12 @@
 ﻿import { createFileRoute } from "@tanstack/react-router";
+import {ExpensesPie} from "@/components/features/analytics/ExpensesPie.tsx";
 
 export const Route = createFileRoute("/_layout/analytics")({
     component: AnalyticsPage,
 });
 
 function AnalyticsPage() {
-    return <div className=""></div>;
+    return <div className="">
+        <ExpensesPie />
+    </div>;
 }
