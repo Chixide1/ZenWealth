2637d878fcda7cf4522db1b37533f4d7c0c475c3
Chikezie <chixide12@gmail.com>
2025-04-25 01:03:22 +0100

    feat: - added docker

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,4 +1,3 @@
-import { scan } from "react-scan";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
@@ -22,11 +21,6 @@ const queryClient = new QueryClient({
         },
 
     },
-});
-
-scan({
-    enabled: true,
-    showToolbar: true,
 });
 
 // Set up a Router instance
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     },
     optimizeDeps: {
         exclude: ["js-big-decimal"]
-    }
+    },
+    base: "/",
 });
 
